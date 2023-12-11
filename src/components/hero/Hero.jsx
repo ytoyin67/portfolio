5797a983af04ce57e3804fcf1e787d698db20f69
Yusuf Toyin <ytoyin67@yahoo.com>
2023-12-11 10:06:40 +0100

    navbar

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,7 +3,7 @@ import './Hero.css'
 
 const Hero = () => {
   return (
-    <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic vel aspernatur enim odit. Saepe accusamus vitae corporis repudiandae beatae itaque, a veniam quos! Sequi quibusdam dolor esse accusantium blanditiis officia ducimus odio sapiente amet in, architecto laboriosam quis numquam. Possimus aut ullam molestiae beatae iste expedita error, debitis corrupti nulla.</div>
+    <div>Hero</div>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,9 +4,7 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <div>
-      <nav>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione, numquam! Illum hic aliquam ad a et accusantium! Architecto laboriosam veritatis nostrum perspiciatis aut exercitationem delectus iure totam! In ipsum culpa deleniti distinctio temporibus odit, quae voluptas harum cupiditate porro odio non et incidunt dignissimos? Odit repudiandae asperiores doloremque nihil commodi.
-      </nav>
+      Navbar
     </div>
   )
 }
